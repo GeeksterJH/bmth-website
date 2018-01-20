9650cf963571593a8aef6dfd89ada0e9e33f10a9
Geekster137 <joren@heyman.io>
2018-01-20 18:58:45 +0100

    Add contact page

---
+++ src/components/Contact.vue
@@ -0,0 +1,50 @@
+<template>
+	<div>
+		<h1>Contact</h1>
+
+		<h2 class="h5">Send Us A Message</h2>
+
+		<form action="POST">
+			<textarea class="form-control" placeholder="Enter your message here..."></textarea>
+			<button type="submit" class="mt-3">Send</button>
+		</form>
+
+		<h2 class="h5 mt-4">Or Contact Us Through</h2>
+
+		<p>Email: bmth-website@gmail.com</p>
+		<p>SMS: 123-456-7890</p>
+		<p>Twitter: @bmth-website</p>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'app-contact'
+	}
+</script>
+
+<style lang="scss" scoped>
+	textarea {
+		resize: none;
+		border-radius: 0;
+	}
+
+	textarea:focus {
+		box-shadow: none;
+	}
+
+	button {
+		border: none;
+		background-color: rgba(172, 172, 172, .33);
+		color: #fff;
+		padding: 5px 10px;
+		border-radius: 6px;
+		transition-duration: 500ms;
+	}
+
+	button:hover {
+		cursor: pointer;
+		background-color: rgba(172, 172, 172, .5);
+	}
+</style>
+
--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import Home from '@/components/Home';
 import About from '@/components/About';
 import Albums from '@/components/Albums';
 import Album from '@/components/Album';
+import Contact from '@/components/Contact';
 
 Vue.use(Router);
 
@@ -28,6 +29,11 @@ export default new Router({
 			path: '/albums/:id',
 			name: 'Album',
 			component: Album
+		},
+		{
+			path: '/contact',
+			name: 'Contact',
+			component: Contact
 		}
 	]
 });
