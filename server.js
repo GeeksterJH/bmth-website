672c86e949c03f7274237c92bda15216c7483a8e
Geekster137 <joren@heyman.io>
2018-01-20 19:38:43 +0100

    Update port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const Koa = require('koa');
 const views = require('koa-views');
 const serve = require('koa-static');
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 let app = new Koa();
 app.use(views('dist', { extension: 'html' }));
